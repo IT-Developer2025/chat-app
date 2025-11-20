175f5a3539abb097d053c3e2374c7c031cb46b88
IT-Developer2025 <yemenideveloper2025@gmail.com>
2025-11-20 06:58:08 +0300

    Connect chat_app to firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/views/home_view.dart';
 import 'package:chat_app/views/login_view.dart';
 import 'package:chat_app/views/signup_view.dart';
 import 'package:chat_app/views/splash_view.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SecureChat());
 }
 
